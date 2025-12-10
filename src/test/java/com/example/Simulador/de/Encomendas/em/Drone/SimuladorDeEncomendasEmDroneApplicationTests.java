35827654682db66493f9878bc83f0b6108d84d6c
Jheyjaygq <ja33278@gmail.com>
2025-12-10 15:55:16 -0300

    first commit

---
+++ src/test/java/com/example/Simulador/de/Encomendas/em/Drone/SimuladorDeEncomendasEmDroneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Simulador.de.Encomendas.em.Drone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladorDeEncomendasEmDroneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
